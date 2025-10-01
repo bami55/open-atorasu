5d7f575e13a0713773d8571ec4c01089db2983aa
bami <1992bami@gmail.com>
2025-10-01 11:10:02 +0900

    累計参加者は直近10回を表示するように変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // 現在の年を自動表示
 document.getElementById('current-year').textContent = new Date().getFullYear();
+
+// 平均参加者数を表示
+const averageParticipants = Array.from(document.querySelectorAll('.bar-value')).reduce((sum, bar) => sum + parseInt(bar.textContent), 0)
+const averageParticipantsAverage = Math.round(averageParticipants / document.querySelectorAll('.bar-value').length);
+document.getElementById('average-participants').textContent = averageParticipantsAverage;
+
+const averageParticipantsCountText = document.querySelector('.stats-section .bar-label').textContent;
+document.getElementById('average-participants-count').textContent = averageParticipantsCountText;
